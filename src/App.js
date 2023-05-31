c860707c0a92383096c6525b95dca1492fd8fd35
Yurii <99128333+CrocodileGenna@users.noreply.github.com>
2023-05-31 21:53:25 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -2,12 +2,6 @@ import { useEffect } from "react";
 import "./App.css";
 const tg = window.Telegram.WebApp;
 function App() {
-  useEffect(() => {
-    tg.ready();
-  }, []);
-  const onClose = () => {
-    tg.close();
-  };
   return (
     <div className="App">
       hello
